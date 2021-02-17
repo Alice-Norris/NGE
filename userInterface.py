da38b75446e687f135b3059c2f6f4a7c79e7f6b8
Alice Kneller <alice.kneller@outlook.com>
2021-02-16 23:06:23 -0500

    UI refined, added sheet view and tile view frames

--- userInterface.py
+++ userInterface.py
@@ -1,8 +1,43 @@
 from tkinter import *
+#creates file menu, menu items, and defines their commands
+def fileMenu(self):
+    self.fileMenu = Menu(self.menuBar, tearoff = 0)
+    self.menuBar.add_cascade(label = 'File', menu=self.fileMenu)
+    self.fileMenu.add_command(label = 'Import image...')
+
+#creates edit menu, menu items, and defines their commands
+def editMenu(self):
+    self.editMenu = Menu(self.menuBar, tearoff = 0)
+    self.menuBar.add_cascade(label = 'Edit', menu=self.editMenu)
+    self.editMenu.add_command(label = 'Preferences...')
+
+#creates help menu, menu items, and defines their commands
+def helpMenu(self):
+    self.helpMenu = Menu(self.menuBar, tearoff = 0)
+    self.menuBar.add_cascade(label = 'Help', menu=self.helpMenu)
+    self.helpMenu.add_command(label = 'About')
+
+def sheetArea(self):
+    self.sheetFrame = Frame(self, borderwidth = 2, relief = GROOVE)
+    self.sheetFrame.grid(column = 0, row = 0)
+    self.sheetDisplay = Canvas(self.sheetFrame, height=320, width=320, borderwidth = 5, relief = SUNKEN, bg='#FFFFFF')
+    self.sheetDisplay.grid()
+    self.sheetLabel = Label(self.sheetFrame, text = "Sheet View")
+    self.sheetLabel.grid()
+
+def tileArea(self):
+    self.tileFrame = Frame(self, borderwidth = 2, relief = GROOVE)
+    self.tileFrame.grid(column =1, row = 0)
+    self.tileDisplay = Canvas(self.tileFrame, height = 320, width = 320, borderwidth = 5, relief = SUNKEN, bg = '#FFFFFF')
+    self.tileDisplay.grid()
+    self.tileLabel = Label(self.tileFrame, text = "Tile View")
+    self.tileLabel.grid()
+    tileDisplay.
 
 class BMP2GBGui(Frame):
     def __init__ (self, master=None):
-        Frame.__init__(self, master, height = 640, width=480)
+        Frame.__init__(self, master, width=800, height = 600, bg='#F0F0F0')
+        self.grid_propagate(0)
         self.grid()
         self.createWidgets()
 
@@ -13,21 +48,15 @@ class BMP2GBGui(Frame):
         #Creating menu along top of window
         self.menuBar = Menu(TLW)
         TLW['menu'] = self.menuBar
-        self.fileMenu = Menu(self.menuBar)
-        self.menuBar.add_cascade(label = 'File', menu=self.fileMenu)
-        self.fileMenu.add_command(label = 'Import image...')
-        #create general program frame
-        self.programFrame = Frame(self, height = 640, width = 480)
-        self.programFrame.grid(column = 0, row = 0)
 
-        #This will be the area where the image is displayed
-        self.imageArea = Canvas(self.programFrame, height=320, width=320, borderwidth = 5, relief = SUNKEN, bg='#FFFFFF')
-        self.imageArea.grid()
+        #call functions to create menus
+        fileMenu(self)
+        editMenu(self)
+        helpMenu(self)
 
-        #This label 
-        self.imageAreaLabel = Label(self.programFrame, text = "Image Area", borderwidth = 2, relief=GROOVE)
-        self.imageAreaLabel.grid()
-
+        #create canvases for sheet and tile views
+        sheetArea(self)
+        tileArea(self)
 #Testin
 BMP2GB = BMP2GBGui()
 BMP2GB.master.title('Bitmap 2 Gameboy')
