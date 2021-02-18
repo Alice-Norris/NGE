ea28684d8ac340d4476651957ba0a47da0f81918
Alice Kneller <alice.kneller@outlook.com>
2021-02-18 05:33:18 -0500

    created function to fill canvases with rectangles, changed grid creation in canvas creation to call new function, reorganized code

--- userInterface.py
+++ userInterface.py
@@ -1,9 +1,46 @@
+from NGEfunc import *
 from tkinter import *
+####                   FUNCTION SECTION                 ####
+#                                                          #
+#   This section contains functions that help the          #
+#   GUI work. Any function that does not place a           #
+#   widget but provides GUI functionality should           #
+#   be placed in here.                                     #
+############################################################
+
+#this function fills the view canvases with rectangles.
+#x defines the number of rectangles across to be drawn across
+#y defines the number of rectangles to be drawn down
+def viewGridGenerator(canvas, x, y):
+    row = 0
+    column = 0
+    rectangles = 0
+    while(row < x):
+        canvas.create_rectangle(row*33, column*33, row*33+33, column*33+33)
+        column += 1
+        if (column == y):
+            column = 0
+            row += 1
+            
+############################################################
+####                END FUNCTION SECTION                ####
+############################################################
+
+####                    MENU SECTION                    ####
+#                                                          #
+#   This section will define menus and link their          #
+#   options to the methods that make them work.            #
+#                                                          #
+############################################################
+
 #creates file menu, menu items, and defines their commands
 def fileMenu(self):
     self.fileMenu = Menu(self.menuBar, tearoff = 0)
     self.menuBar.add_cascade(label = 'File', menu=self.fileMenu)
+    self.fileMenu.add_command(label = 'Save')
+    self.fileMenu.add_command(label = 'Save As...')
     self.fileMenu.add_command(label = 'Import image...')
+    self.fileMenu.add_command(label = 'Open Sheet ...')
     self.fileMenu.add_command(label = "Quit", command=self.quit)
 
 #creates edit menu, menu items, and defines their commands
@@ -17,40 +54,27 @@ def helpMenu(self):
     self.helpMenu = Menu(self.menuBar, tearoff = 0)
     self.menuBar.add_cascade(label = 'Help', menu=self.helpMenu)
     self.helpMenu.add_command(label = 'About')
+############################################################
+####                    END MENU SECTION                ####
+############################################################
 
 def sheetArea(self):
-    self.sheetFrame = Frame(self, borderwidth = 2, relief = GROOVE)
-    self.sheetFrame.grid(column = 0, row = 0)
-    self.sheetDisplay = Canvas(self.sheetFrame, height=256, width=512, bg='#FFFFFF', border=0, borderwidth = 0, highlightthickness=0)
-    self.sheetDisplay.grid()
+    self.sheetFrame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = "Sheet View", padx=5, pady=5)
+    self.sheetFrame.grid(column = 0, row=0, padx=5, pady=5)
+    self.sheetDisplay = Canvas(self.sheetFrame, height=266, width=530, bg='#FFFFFF', border=0, borderwidth = 0, highlightthickness=0)
+    self.sheetDisplay.grid(sticky = 's')
     self.sheetDisplay.grid_propagate(0)
-    self.sheetLabel = Label(self.sheetFrame, text = "Sheet View")
-    self.sheetLabel.grid()
-    row = -1
-    column = -1
-    while(row <= 15 and column <= 8):
-        self.sheetDisplay.create_rectangle(row*32, column*32, row*32+31, column*32+31)
-        column += 1
-        if (column == 8):
-            column = 0
-            row += 1
+    viewGridGenerator(self.sheetDisplay, 16, 8)
         
 def tileArea(self):
-    self.tileFrame = Frame(self, borderwidth = 2, relief = GROOVE)
-    self.tileFrame.grid(column =1, row = 0)
+    self.tileFrame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Tile View', padx=5, pady=5)
+    self.tileFrame.grid(column = 1, row = 0, padx = 5, pady = 5)
     self.tileDisplay = Canvas(self.tileFrame, height = 266, width = 266, bg = '#FFFFFF', borderwidth = 0, highlightthickness=0)
     self.tileDisplay.grid()
-    self.tileLabel = Label(self.tileFrame, text = "Tile View")
-    self.tileLabel.grid()
     row = 0
     column = 0
     characters=[]
-    while (row < 8):
-        self.tileDisplay.create_rectangle(column*33, row*33, column*33+33, row*33+33)
-        column += 1
-        if(column == 8):
-            column = 0
-            row += 1
+    viewGridGenerator(self.tileDisplay, 8, 8)
 
             
 class BMP2GBGui(Frame):
