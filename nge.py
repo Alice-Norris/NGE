7ad86f2c696f5cc1c69ab3bb1a36c95a7dde9c4f
Alice Kneller <alice.kneller@outlook.com>
2021-02-19 19:23:55 -0500

    bruh idk, made the style PEP compliant, added color palette, some other stuff (???)

---
+++ nge.py
@@ -0,0 +1,82 @@
+####                    NGE PROGRAM                     ####
+# This file includes the methods that power the NGE,       #
+# and also holds data structures                           #
+############################################################
+
+####                    NGE FUNCTIONS                   ####
+# This section contains the functions that power the NGE   #
+############################################################
+
+def create_2d_array(width, height):
+    width = width
+    height = height
+    posx = 0
+    posy = 0
+    array = []
+    while (posx < width):
+        array.append([])
+        posx += 1
+    posx = 0
+    for column in array:
+        while (posy < height):
+            coordinate = str(posx) + ", " + str(posy)
+            column.append(coordinate)
+            posy += 1
+        posx +=1
+        posy = 0
+    return array
+
+
+############################################################
+####              END NGE FUNCTION SECTION              ####
+############################################################
+
+####                     NGE CLASSES                    ####
+# This section contains the classes for the NGE            #
+############################################################
+
+#This class is used to store all temporary program data.
+class Workspace:
+    def __init__(cls):
+        cls.current_character = None
+        cls.current_sheet = None
+        cls.current_book = None
+        cls.current_tool = None
+
+#this class is used to describe the character data for a single object
+class Character: 
+    def __init__(cls, object_num, sheet):
+        cls.object_num = object_num #this should be a hex number
+        cls.sheet = sheet # this is sheet number is for program purposes and does not (yet) relate to the output data
+        cls.data = create_2d_array(8, 8) #8x8 list, used to store individual pixel information. Should be 0 to 3.
+
+class Sheet:
+    def __init__(cls, sheet_num):
+        cls.sheet_num = sheet_num #This integer identifies the number of the sheet, which holds individual characters. Sheets are stored in Books
+        cls.character_array = create_2d_array(16, 8) #16x8 list, used to store characters.
+
+    def add_character(cls, character, x, y):
+        cls.character_array[x][y] = character #adds given character at the x, y position provided
+
+    def delete_character(cls, character, x, y): #sets given position in character list to 0, overwriting the character.
+        cls.character_array[x][y] = 0
+
+    def add_rectangle(cls, rectangle, x, y): #adds a rectangle canvas object to the two-dimensional list of rectangles.
+        cls.rectangle_array[x][y] = rectangle
+
+class Book:
+    def __init___(cls, book_name):
+        cls.book_name = book_name #This string is the name of the book. Books hold sheets.
+        cls.sheet_array = [] #This list contains the Sheet objects that it is a part of.
+############################################################
+####               END NGE CLASSES SECTION              ####
+############################################################
+
+
+character1 = Character(0, 1)
+
+sheet1 = Sheet(0)
+
+sheet1.add_character(character1, 0, 0)
+
+print(sheet1.character_array[0][0].data[7][7])
--- userInterface.py
+++ userInterface.py
@@ -1,4 +1,4 @@
-from NGEfunc import *
+from nge import Character, Sheet, Book, create_2d_array
 from tkinter import *
 ####                   FUNCTION SECTION                 ####
 #                                                          #
@@ -11,10 +11,10 @@ from tkinter import *
 #this function fills the view canvases with rectangles.
 #x defines the number of rectangles across to be drawn across
 #y defines the number of rectangles to be drawn down
-def viewGridGenerator(canvas, x, y):
+def view_grid_generator(canvas, x, y):
     row = 0
     column = 0
-    rectangles = create2dArray(x, y)
+    rectangles = create_2d_array(x, y)
     while(row < x):
         canvas.create_rectangle(row*33, column*33, row*33+33, column*33+33, )
         column += 1
@@ -22,6 +22,10 @@ def viewGridGenerator(canvas, x, y):
             column = 0
             row += 1
         
+def create_color_canvas(self, frame, color):
+    #self.tile_display = Canvas(self.tile_frame, height = 266, width = 266, bg = '#FFFFFF', borderwidth = 0, highlightthickness=0)
+    self.tile_display = Canvas(frame, height = 32, width = 32, bg = color, borderwidth = 3, relief = 'sunken')
+    return self.tile_display
 
 ############################################################
 ####                END FUNCTION SECTION                ####
@@ -35,34 +39,60 @@ def viewGridGenerator(canvas, x, y):
 ############################################################
 
 #creates file menu, menu items, and defines their commands
-def fileMenu(self):
-    self.fileMenu = Menu(self.menuBar, tearoff = 0)
-    self.sheetsMenu = Menu(self.fileMenu, tearoff=0)
-    self.fileMenu.add_cascade(label = 'Sheets', menu=self.sheetsMenu)
-    self.menuBar.add_cascade(label = 'File', menu=self.fileMenu)
-    self.fileMenu.add_command(label = 'Save')
-    self.fileMenu.add_command(label = 'Save As...')
-    self.fileMenu.add_command(label = 'Import image...')
-    self.fileMenu.add_command(label = "Quit", command=self.quit)
+def file_menu(self):
+    self.file_menu = Menu(self.menu_bar, tearoff = 0)
+    self.sheets_menu = Menu(self.file_menu, tearoff=0)
+    self.file_menu.add_cascade(label = 'Sheets', menu=self.sheets_menu)
+    self.menu_bar.add_cascade(label = 'File', menu=self.file_menu)
+    self.file_menu.add_command(label = 'Save')
+    self.file_menu.add_command(label = 'Save As...')
+    self.file_menu.add_command(label = 'Import image...')
+    self.file_menu.add_command(label = "Quit", command=self.quit)
 
     #creates sheet cascade, adds to file menu
-    self.sheetsMenu.add_command(label = 'New Sheet')
-    self.sheetsMenu.add_command(label = 'Open Sheet...')
-    self.sheetsMenu.add_command(label = 'Delete Sheet...')
+    self.sheets_menu.add_command(label = 'New Sheet')
+    self.sheets_menu.add_command(label = 'Open Sheet...')
+    self.sheets_menu.add_command(label = 'Delete Sheet...')
 
 #creates edit menu, menu items, and defines their commands
-def editMenu(self):
-    self.editMenu = Menu(self.menuBar, tearoff = 0)
-    self.menuBar.add_cascade(label = 'Edit', menu=self.editMenu)
-    self.editMenu.add_command(label = 'Preferences...')
+def edit_menu(self):
+    self.edit_menu = Menu(self.menu_bar, tearoff = 0)
+    self.menu_bar.add_cascade(label = 'Edit', menu=self.edit_menu)
+    self.edit_menu.add_command(label = 'Preferences...')
 
 #creates help menu, menu items, and defines their commands
-def helpMenu(self):
-    self.helpMenu = Menu(self.menuBar, tearoff = 0)
-    self.menuBar.add_cascade(label = 'Help', menu=self.helpMenu)
-    self.helpMenu.add_command(label = 'About')
+def help_menu(self):
+    self.help_menu = Menu(self.menu_bar, tearoff = 0)
+    self.menu_bar.add_cascade(label = 'Help', menu=self.help_menu)
+    self.help_menu.add_command(label = 'About')
 ############################################################
 ####                  END MENU SECTION                  ####
+############################################################
+
+####                   FRAME SECTION                    ####
+#                                                          #
+#   This section will define menus and link their          #
+#   options to the methods that make them work.            #
+#                                                          #
+############################################################
+def create_frames(self):
+    self.sheet_frame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = "Sheet View", padx=5, pady=5)
+    self.sheet_frame.grid(column = 0, row=0, padx=5, pady=5, sticky = 'n')
+
+    self.tile_frame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Character View', padx=5, pady=5)
+    self.tile_frame.grid(column = 1, row = 0, padx = 5, pady = 5, sticky = 'n')
+
+    self.tool_frame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Tools', padx = 5, pady = 5)
+    self.tool_frame.grid(column = 2, row = 0, padx=5, pady=5, sticky = 'n')
+
+    self.color_frame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Colors', padx=5, pady=5)
+    self.color_frame.grid(column = 3, row = 0, padx=5, pady=5, sticky = 'n')
+    self.current_color_frame = LabelFrame(self.color_frame, labelanchor='n', borderwidth = 0, text='Current', padx = 5, pady = 5)
+    self.current_color_frame.grid()
+    self.palette_frame = LabelFrame(self.color_frame, labelanchor = 'n', borderwidth = 0, text= 'Palette', padx=5, pady=5)
+    self.palette_frame.grid()
+############################################################
+####                  END FRAME SECTION                 ####
 ############################################################
 
 ####                   WIDGET SECTION                   ####
@@ -71,35 +101,41 @@ def helpMenu(self):
 #   options to the methods that make them work.            #
 #                                                          #
 ############################################################
-def sheetArea(self):
-    self.sheetFrame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = "Sheet View", padx=5, pady=5)
-    self.sheetFrame.grid(column = 0, row=0, padx=5, pady=5)
-    self.sheetDisplay = Canvas(self.sheetFrame, height=266, width=530, bg='#FFFFFF', border=0, borderwidth = 0, highlightthickness=0)
-    self.sheetDisplay.grid(sticky = 's')
-    viewGridGenerator(self.sheetDisplay, 16, 8)
+def sheet_area(self):
+    self.sheet_display = Canvas(self.sheet_frame, height=266, width=530, bg='#FFFFFF', border=0, borderwidth = 0, highlightthickness=0)
+    self.sheet_display.grid(sticky = 's')
+    view_grid_generator(self.sheet_display, 16, 8)
         
-def tileArea(self):
-    self.tileFrame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Tile View', padx=5, pady=5)
-    self.tileFrame.grid(column = 1, row = 0, padx = 5, pady = 5)
-    self.tileDisplay = Canvas(self.tileFrame, height = 266, width = 266, bg = '#FFFFFF', borderwidth = 0, highlightthickness=0)
-    self.tileDisplay.grid()
+def tile_area(self):
+    self.tile_display = Canvas(self.tile_frame, height = 266, width = 266, bg = '#FFFFFF', borderwidth = 0, highlightthickness=0)
+    self.tile_display.grid()
     row = 0
     column = 0
     characters=[]
-    viewGridGenerator(self.tileDisplay, 8, 8)
+    view_grid_generator(self.tile_display, 8, 8)
 
-def toolArea(self):
-    self.toolFrame = LabelFrame(self, labelanchor = 's', borderwidth = 2, text = 'Tools', padx = 5, pady = 5)
-    self.toolFrame.grid(column = 2, row = 0, padx=5, pady=5, sticky = 'n')
-    self.pencilImage = BitmapImage(file='pencil.xbm', foreground ='black', background='white')
-    self.eraserImage = BitmapImage(file='eraser.xbm', foreground = 'black', background = 'white')
-    self.bucketImage = BitmapImage(file = 'bucket.xbm', foreground = 'black', background = 'white')
-    self.pencilButton = Button(self.toolFrame, image=self.pencilImage, width=64, height=64)
-    self.pencilButton.grid()
-    self.eraserButton = Button(self.toolFrame, image=self.eraserImage, width=64, height = 64)
-    self.eraserButton.grid()
-    self.bucketButton = Button(self.toolFrame, image = self.bucketImage, width = 64, height = 64)
-    self.bucketButton.grid()
+def tool_area(self):
+    self.pencil_image = BitmapImage(file='pencil.xbm', foreground ='black', background='white')
+    self.eraser_image = BitmapImage(file='eraser.xbm', foreground = 'black', background = 'white')
+    self.bucket_image = BitmapImage(file = 'bucket.xbm', foreground = 'black', background = 'white')
+    self.pencil_button = Button(self.tool_frame, image=self.pencil_image, width=64, height=64)
+    self.pencil_button.grid()
+    self.eraser_button = Button(self.tool_frame, image=self.eraser_image, width=64, height = 64)
+    self.eraser_button.grid()
+    self.bucket_button = Button(self.tool_frame, image = self.bucket_image, width = 64, height = 64)
+    self.bucket_button.grid()
+    
+def color_area(self):
+    self.black_swatch = create_color_canvas(self, self.palette_frame, '#000000')
+    self.black_swatch.grid(pady=5)
+    self.dk_gray_swatch = create_color_canvas(self, self.palette_frame, '#575757')
+    self.dk_gray_swatch.grid(pady=5)
+    self.lt_gray_swatch = create_color_canvas(self, self.palette_frame, '#ababab')
+    self.lt_gray_swatch.grid(pady=5)
+    self.white = create_color_canvas(self, self.palette_frame, '#ffffff')
+    self.white.grid(pady=5)
+    self.current_color = create_color_canvas(self, self.current_color_frame, '#ffffff')
+    self.current_color.grid()
 ############################################################
 ####                 END WIDGET SECTION                 ####
 ############################################################
@@ -109,25 +145,30 @@ class BMP2GBGui(Frame):
         Frame.__init__(self, master, width=1024, height = 512, bg='#F0F0F0')
         self.grid_propagate(0)
         self.grid()
-        self.createWidgets()
+        self.create_widgets()
 
-    def createWidgets(self):
+    def create_widgets(self):
         #setting top level window to a variable
         TLW = self.winfo_toplevel()
 
         #Creating menu along top of window
-        self.menuBar = Menu(TLW)
-        TLW['menu'] = self.menuBar
+        self.menu_bar = Menu(TLW)
+        TLW['menu'] = self.menu_bar
 
         #call functions to create menus
-        fileMenu(self)
-        editMenu(self)
-        helpMenu(self)
+        file_menu(self)
+        edit_menu(self)
+        help_menu(self)
+
+        #call function to create frams
+        create_frames(self)
 
         #create canvases for sheet and tile views
-        sheetArea(self)
-        tileArea(self)
-        toolArea(self)
+        sheet_area(self)
+        tile_area(self)
+        tool_area(self)
+
+        color_area(self)
 #Testin
 BMP2GB = BMP2GBGui()
 BMP2GB.master.title('Bitmap 2 Gameboy')
